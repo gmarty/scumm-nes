49d70adab1a3eea208e2a184b761e26524cdae1b
edo999 <edo999@gmail.com>
2024-05-11 17:14:09 +0100

    Animate the menu on small screens 📲.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,11 @@
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Dialog } from '@headlessui/react';
+import {
+  Dialog,
+  DialogPanel,
+  Transition,
+  TransitionChild,
+} from '@headlessui/react';
 import {
   Cog8ToothIcon,
   Bars3Icon,
@@ -66,49 +71,71 @@ export default function Header() {
           </Link>
         </div>
       </nav>
-      <Dialog
-        as="div"
-        className="md:hidden"
-        open={mobileMenuOpen}
-        onClose={setMobileMenuOpen}>
-        <div className="fixed inset-0 z-10 bg-slate-100/75 dark:bg-slate-900/75" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-slate-100 px-3 py-2 pl-6 sm:max-w-sm sm:ring-1 sm:ring-white/10 dark:bg-slate-900">
-          <div className="flex items-center justify-between">
-            <span className="sr-only">SCUMM NES resource explorer</span>
-            <img
-              className="h-6 w-auto"
-              src={meteor}
-              alt="Maniac Mansion meteor"
-            />
-            <button
-              type="button"
-              className="-m-2 rounded-md p-2 text-slate-500 hover:text-slate-800 hover:dark:text-slate-200"
-              onClick={() => setMobileMenuOpen(false)}>
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon
-                strokeWidth="2"
-                className="size-6"
-                aria-hidden="true"
-              />
-            </button>
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-slate-500/25">
-              <div className="space-y-2 py-6">
-                {navigation.map((item) => (
-                  <Link
-                    key={item.name}
-                    to={item.href}
-                    className="-mx-3 block rounded px-3 py-2 text-base font-semibold leading-7 text-slate-700 hover:bg-slate-200 dark:text-slate-300 hover:dark:bg-slate-800"
-                    onClick={() => setMobileMenuOpen(false)}>
-                    {item.name}
-                  </Link>
-                ))}
+      <Transition
+        show={mobileMenuOpen}
+        as={Fragment}>
+        <Dialog
+          as="div"
+          className="md:hidden"
+          onClose={setMobileMenuOpen}>
+          <TransitionChild
+            as={Fragment}
+            enter="transition-opacity ease-linear duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity ease-linear duration-300"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0">
+            <div className="fixed inset-0 z-10 bg-slate-100/75 dark:bg-slate-900/75" />
+          </TransitionChild>
+
+          <TransitionChild
+            as={Fragment}
+            enter="transition ease-in-out duration-300 transform"
+            enterFrom="translate-x-full"
+            enterTo="translate-x-0"
+            leave="transition ease-in-out duration-300 transform"
+            leaveFrom="translate-x-0"
+            leaveTo="translate-x-full">
+            <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-slate-100 px-3 py-2 pl-6 sm:max-w-sm dark:bg-slate-900">
+              <div className="flex items-center justify-between">
+                <span className="sr-only">SCUMM NES resource explorer</span>
+                <img
+                  className="h-6 w-auto"
+                  src={meteor}
+                  alt="Maniac Mansion meteor"
+                />
+                <button
+                  type="button"
+                  className="-m-2 rounded-md p-2 text-slate-500 hover:text-slate-800 hover:dark:text-slate-200"
+                  onClick={() => setMobileMenuOpen(false)}>
+                  <span className="sr-only">Close menu</span>
+                  <XMarkIcon
+                    strokeWidth="2"
+                    className="size-6"
+                    aria-hidden="true"
+                  />
+                </button>
               </div>
-            </div>
-          </div>
-        </Dialog.Panel>
-      </Dialog>
+              <div className="mt-6 flow-root">
+                <div className="-my-6 divide-y divide-slate-500/25">
+                  <div className="space-y-2 py-6">
+                    {navigation.map((item) => (
+                      <Link
+                        key={item.name}
+                        to={item.href}
+                        className="-mx-3 block rounded px-3 py-2 text-base font-semibold leading-7 text-slate-700 hover:bg-slate-200 dark:text-slate-300 hover:dark:bg-slate-800"
+                        onClick={() => setMobileMenuOpen(false)}>
+                        {item.name}
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </Dialog>
+      </Transition>
     </header>
   );
 }
