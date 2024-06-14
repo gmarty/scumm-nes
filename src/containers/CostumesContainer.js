88c697f38fc3a39aad8955db8859b605c657706d
Gamaiel Zavala <gamaiel@yahoo.com>
2024-06-14 04:49:31 -0600

    Partial fix for costume parsing (#21)
    * Partial fix for costume parsing
    
    * A few refinements

--- src/containers/CostumesContainer.js
+++ src/containers/CostumesContainer.js
@@ -5,6 +5,7 @@ import Main from '../components/Main';
 import MainHeader from '../components/MainHeader';
 import ResourceMetadata from '../components/ResourceMetadata';
 import CostumeCanvasContainer from './CostumeCanvasContainer';
+import { hex } from '../lib/utils';
 
 const CostumesContainer = ({
   costumegfx,
@@ -21,15 +22,15 @@ const CostumesContainer = ({
   const costume =
     costumes.find(({ metadata }) => metadata.id === currentId) || null;
 
-  // console.log('costumegfx', costumegfx);
+  //console.log('costumegfx', costumegfx);
   console.log('costumes', costumes);
-  console.log('sprpals', sprpals);
-  console.log('sprdesc', sprdesc);
-  console.log('sproffs', sproffs);
-  console.log('sprlens', sprlens);
-  console.log('sprdata', sprdata);
+  //console.log('sprpals', sprpals);
+  console.log('sprdesc', sprdesc[0].sprdesc.map((v) => hex(v, 4)));
+  console.log('sproffs', sproffs[0].sproffs.map((v) => hex(v, 4)));
+  console.log('sprlens', sprlens[0].sprlens);
+  console.log('sprdata', sprdata[0].sprdata);
 
-  console.log('costume', costume);
+  //console.log('costume', costume);
 
   if (!costume) {
     return null;
@@ -45,25 +46,26 @@ const CostumesContainer = ({
       </PrimaryColumn>
 
       <Main>
-        <MainHeader title={`Costume ${currentId}`}>
+        <MainHeader title={`Costumes`}>
           <ResourceMetadata metadata={costume.metadata} />
         </MainHeader>
         {costume.costumes.map((costume, i) => (
           <div key={i}>
-            <h2>{`Costume ${i}`}</h2>
+            <h2>{`Costume ${i+1}`}</h2>
             <div className="flex flex-row gap-4">
               {costume.map((frame, j) => (
                 <div key={j}>
-                  <h3>{`Frame ${frame}`}</h3>
+                  <h3 class="text-xs">{`Frame ${j}`}</h3>
                   <CostumeCanvasContainer
-                    frame={frame}
+                    cIndex={i}
+                    frame={j}
                     gfx={costumegfx[0]}
                     sprdesc={sprdesc[0].sprdesc}
                     sproffs={sproffs[0].sproffs}
                     sprlens={sprlens[0].sprlens}
                     sprdata={sprdata[0].sprdata}
                     sprpals={sprpals[0]}
-                    zoom={4}
+                    zoom={2}
                   />
                 </div>
               ))}
